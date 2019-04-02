3701dde0b18ca338c3ff2233034da807ea56a527
Max Stepanov <79137837220@yandex.ru>
2019-04-02 11:16:17 +0700

    develop/small refactor

--- src/components/Headline/spec.js
+++ src/components/Headline/spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import checkPropTypes from 'check-prop-types';
 import Headline from './index';
 import { checkElemetnts, propsError } from '../../utils';
 
